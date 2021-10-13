4073b67b93ba51ee0c8a5e60d4e3acb0bc881f01
FayezR <64635876+ExtremeC0der@users.noreply.github.com>
2021-10-13 11:32:04 +0100

    Made changes to value_iteration method)

--- main.py
+++ main.py
@@ -1,81 +1,80 @@
-from abc import ABC, abstractmethod
-from dice_game import DiceGame
-import numpy as np
-
 from examplealgorithms import DiceGameAgent
+from numpy import absolute
 
 
 class MyAgent(DiceGameAgent):
     def __init__(self, game):
         super().__init__(game)
         self.gamma = 1
-        self.theta = 0.1
-        self.value = self.value_iteration()
+        self.theta = 0.99
 
-    def find_next_step(self, value, state):
-
-        #  Creates a dictionary to store actions (key) and probability*reward (value)
-        actions_dict = {}
-        for single_action in self.game.actions:
-            actions_dict.update({single_action : 0})
-
-        # Creates a list of actions from the dictionaru
-        actions = actions_dict.keys()
-
-        # For each single_action from the list of actions
-        for single_action in actions:
-            next_states, game_over, current_reward, probabilities = self.game.get_next_states(single_action, state)
-
-            # For each next_state and associated probability of that action & given state
-            for next_state, probability in zip(next_states, probabilities):
-                if  next_state == None :  # if there is no next_state then multiply probability by reward of the current state
-                    x = probability * current_reward
-                    actions_dict[single_action] += x
-
-                else:  # Calculate Bellman Update if there is a next state
-                    bellman = probability * (current_reward + self.gamma * value[next_state][0])
-                    actions_dict[single_action] += bellman
-
-        return actions_dict
+        # As part of pre-processing, a dictionary of states (key) and the expected reward and optimal policy [
+        # expected reward, optimal policy] (key) is created. This dictionary is used to store the optimal policy for
+        # any given state
+        self.policies = self.value_iteration()
 
     def value_iteration(self):
 
-        # Initialise dictionary with each state as keys and values [expected reward, optimal policy]
-        value = {}
+        # Initialises dictionary of states (key) and the expected reward and optimal policy
+        # [expected reward, optimal policy] (value)
+        policies = {}
         for state in self.game.states:
-            value.update({state: [0, None]})
-        # Loop until delta < self.theta
+            policies.update({state: [0, None]})
+
         while True:
             delta = 0
 
-            # For each state
             for state in self.game.states:
-                # Get the list of reward values pertaining to each action given that state
-                actions = self.find_next_step(value, state)
+                actions_dict = self.find_next_step(policies, state)
 
-                # Take the highest expected reward value
-                highest_action_reward = max(actions.values())
+                v = actions_dict.values()  # Creates a list of expected reward values for a given action
+                highest_action_reward = max(v)  # Finds the biggest reward from the list of expected reward values
 
-                # Update delta with either delta or the absolute difference between the
-                # new maximum value for that state and the previous maximum value for that state
-                delta = max(delta, np.abs(highest_action_reward - value[state][0]))
+                # Stores the absolute difference between new max reward value and previous max reward value for a state
+                x = absolute(highest_action_reward - policies[state][0])
+                delta = max(delta, x)  # delta is updated with the maximum value of delta or x
 
-                # Update the maximum value and optimal policy for that state
-                value[state] = [highest_action_reward, max(actions, key=actions.get)]
+                y = max(actions_dict, key=actions_dict.get)
+                policies[state] = [highest_action_reward, y]
 
-            # If the change is less than self.theta, end the loop
-            if delta < self.theta:
+            if self.theta > delta:
                 break
-        return value
+
+        return policies
+
+    def find_next_step(self, policies, state):
+
+        #  Creates a dictionary to store actions (key) and their expected reward (probability * reward) (value)
+        actions_dict = {}
+        for n in self.game.actions:
+            actions_dict.update({n: 0})
+
+        actions = actions_dict.keys()  # generates a list of actions from the dictionary
+
+        for action in actions:
+            next_states, game_over, current_reward, probabilities = self.game.get_next_states(action, state)
+
+            for x in range(len(next_states)):
+                probability = probabilities[x]
+                next_state = next_states[x]
+
+                if next_state is None:  # if no next_state, probability is multiplied by reward of the current state
+                    x = probability * current_reward
+                    actions_dict[action] = actions_dict[action] + x
+
+                else:  # i.e. if there is a next_state, the Bellman Update is calculated
+                    bellman = probability * (current_reward + self.gamma * policies[next_state][0])
+                    actions_dict[action] = actions_dict[action] + bellman
+
+        return actions_dict
 
     def play(self, state):
         """
-        For the given state, obtains the optimal policy
-
-        param state : the current state of the game
-
-        returns self.V[state][1] : the optimal action associated with that state
+        Function returns an optimal action for a given state. 
+        The function looks up the dictionary value for a given state returns the optimal policy.
+        The dictionary is set up to store a list of values for a given state.
+        :policies[state][0]   returns the expected reward for an action
+        :policies[state][1]   returns the optimal policy for the state
         """
 
-        return self.value[state][1]
-
+        return self.policies[state][1]
